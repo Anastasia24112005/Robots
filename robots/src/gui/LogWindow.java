10e848f10ba08aa6ab0fc1eccf2073a656ec7f4a
Anastasia <anastasia200524@icloud.com>
2025-05-13 12:17:24 +0500

    2 задача

--- robots/src/gui/LogWindow.java
+++ robots/src/gui/LogWindow.java
@@ -1,27 +1,19 @@
 package gui;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-import java.awt.TextArea;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.Properties;
-
+import java.awt.*;
 import javax.swing.JInternalFrame;
 import javax.swing.JPanel;
-
 import log.LogChangeListener;
 import log.LogEntry;
 import log.LogWindowSource;
+import java.awt.BorderLayout;
+import java.awt.Rectangle;
+import java.util.prefs.Preferences;
 
-public class LogWindow extends JInternalFrame implements LogChangeListener {
+public class LogWindow extends JInternalFrame implements LogChangeListener, WindowState {
+    private final Preferences prefs = Preferences.userNodeForPackage(LogWindow.class);
     private LogWindowSource m_logSource;
     private TextArea m_logContent;
-    private static final String CONFIG_FILE = System.getProperty("user.home") + File.separator + "log_window_config.properties";
 
     public LogWindow(LogWindowSource logSource) {
         super("Протокол работы", true, true, true, true);
@@ -29,13 +21,12 @@ public class LogWindow extends JInternalFrame implements LogChangeListener {
         m_logSource.registerListener(this);
         m_logContent = new TextArea("");
         m_logContent.setSize(200, 500);
-
         JPanel panel = new JPanel(new BorderLayout());
         panel.add(m_logContent, BorderLayout.CENTER);
         getContentPane().add(panel);
         pack();
+        load();
         updateLogContent();
-        loadState();
     }
 
     private void updateLogContent() {
@@ -52,41 +43,21 @@ public class LogWindow extends JInternalFrame implements LogChangeListener {
         EventQueue.invokeLater(this::updateLogContent);
     }
 
-    public void saveState() {
-        Properties properties = new Properties();
-        properties.setProperty("x", String.valueOf(getX()));
-        properties.setProperty("y", String.valueOf(getY()));
-        properties.setProperty("width", String.valueOf(getWidth()));
-        properties.setProperty("height", String.valueOf(getHeight()));
-        properties.setProperty("maximized", String.valueOf(isMaximum()));
-
-        try (OutputStream output = new FileOutputStream(CONFIG_FILE)) {
-            properties.store(output, null);
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
+    @Override
+    public void save() {
+        Rectangle bounds = getBounds();
+        prefs.putInt("LogWindow_X", bounds.x);
+        prefs.putInt("LogWindow_Y", bounds.y);
+        prefs.putInt("LogWindow_WIDTH", bounds.width);
+        prefs.putInt("LogWindow_HEIGHT", bounds.height);
     }
 
-    public void loadState() {
-        Properties properties = new Properties();
-        try (InputStream input = new FileInputStream(CONFIG_FILE)) {
-            properties.load(input);
-            int x = Integer.parseInt(properties.getProperty("x", String.valueOf(getX())));
-            int y = Integer.parseInt(properties.getProperty("y", String.valueOf(getY())));
-            int width = Integer.parseInt(properties.getProperty("width", String.valueOf(getWidth())));
-            int height = Integer.parseInt(properties.getProperty("height", String.valueOf(getHeight())));
-            boolean maximized = Boolean.parseBoolean(properties.getProperty("maximized", String.valueOf(isMaximum())));
-
-            setBounds(x, y, width, height);
-            if (maximized) {
-                try {
-                    setMaximum(true);
-                } catch (java.beans.PropertyVetoException e) {
-                    e.printStackTrace();
-                }
-            }
-        } catch (IOException ex) {
-            System.out.println("Configuration file not found. Using default settings.");
-        }
+    @Override
+    public void load() {
+        int x = prefs.getInt("LogWindow_X", 10);
+        int y = prefs.getInt("LogWindow_Y", 10);
+        int width = prefs.getInt("LogWindow_WIDTH", 300);
+        int height = prefs.getInt("LogWindow_HEIGHT", 800);
+        setBounds(x, y, width, height);
     }
 }
